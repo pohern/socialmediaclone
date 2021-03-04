ad7c7e1a8fe1c90aeae5987336679725b3a9b99b
pohern <ohern_p@yahoo.com>
2021-03-04 15:22:06 -0600

    move typeDefs to own folder

--- index.js
+++ index.js
@@ -3,21 +3,11 @@ const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-
+const typeDefs = require('./graphql/typeDefs')
 const Post = require('./models/Post')
 
 
-const typeDefs = gql`
-    type Post{
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-    }
-  type Query {
-    getPosts: [Post]
-  }
-`;
+
 
 const resolvers = {
   Query: {
