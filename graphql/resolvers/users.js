5b40ec8c64cdfe3b4dc3640fd6a34968bc7667d7
pohern <ohern_p@yahoo.com>
2021-03-05 11:38:56 -0600

    install bcryptjs to hash passwords, and jswebtoken to authenticate

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,12 @@
+const User = require('../../models/User')
+
+module.exports = {
+    Mutation: {
+        register(_, args, context, info){
+            // TODO: Validate User data
+            // TODO: Make sure user doesnt already exist
+            // TODO: hash password and create an auth token
+            
+        }
+    }
+}
