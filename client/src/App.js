7ee493c471c0bf4dccc40390448bc13db7b0fe60
pohern <ohern_p@yahoo.com>
2021-03-05 21:22:26 -0600

    add navbar to app

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,16 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
+
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
   return (
     <Router>
+      <NavBar />
       <Route exact path='/' component={Home} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/register' component={Register} />
