575e1b2cfee6e54166fc17abe05ef487c7d55f43
pohern <ohern_p@yahoo.com>
2021-03-06 10:31:13 -0600

    mplemetn authentication on whole site

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container } from 'semantic-ui-react'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
-import 'semantic-ui-css/semantic.min.css'
-import './App.css';
+import { AuthProvider } from "./context/auth";
 
-
-import NavBar from './components/NavBar'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <Router>
-      <Container>
-        <NavBar />
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/register' component={Register} />
-      </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+          <NavBar />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
