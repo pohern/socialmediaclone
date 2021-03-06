68bc2f335d6952f07eee917844c8039e4191d3cd
pohern <ohern_p@yahoo.com>
2021-03-05 20:21:59 -0600

    finsih server side set up

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,6 +1,6 @@
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 module.exports = {
   Query: {
@@ -38,6 +38,10 @@ module.exports = {
 
       const post = await newPost.save();
 
+      context.pubsub.publish('NEW_POST',{
+          newPost: post
+      })
+      
       return post;
     },
     async deletePost(_, { postId }, context) {
@@ -55,5 +59,30 @@ module.exports = {
         throw new Error(err);
       }
     },
+    async likePost(_, { postId }, context){
+        const { username } = checkAuth(context)
+
+        const post = await Post.findById(postId)
+
+        if(post){
+            if(post.likes.find(like => like.username === username)){
+                //Post already liked, unlike it
+                post.likes = post.likes.filter(like => like.username !== username)
+            } else {
+                //not liked,  like post
+                post.likes.push({
+                    username,
+                    createdAt: new Date().toISOString()
+                })
+            }
+            await post.save();
+            return post
+        } else throw new UserInputError('Post not found')
+    }
   },
+  Subscription: {
+      newPost: {
+          suscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
+      }
+  }
 };
