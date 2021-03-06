e4fe66496aebf252a24221f3217cd508f68cf876
pohern <ohern_p@yahoo.com>
2021-03-06 10:13:22 -0600

    implement custom hook on register page;

--- client/src/util/hooks.js
+++ client/src/util/hooks.js
@@ -3,10 +3,20 @@ import { useState } from 'react'
 
 export const useForm = (callback, initalState = {}) => {
 
-    const [values, setValues] = useState();
-
+    const [values, setValues] = useState(initalState);
 
     const onChange = (e) => {
       setValues({ ...values, [e.target.name]: e.target.value });
     };
+
+    const onSubmit = e => {
+        e.preventDefault()
+        callback()
+    }
+
+    return {
+        onChange,
+        onSubmit,
+        values
+    }
 }
