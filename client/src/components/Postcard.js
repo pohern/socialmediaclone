5f835b73b1c41845a045a5137cfece9b37732810
pohern <ohern_p@yahoo.com>
2021-03-06 09:15:04 -0600

    add comment button

--- client/src/components/Postcard.js
+++ client/src/components/Postcard.js
@@ -9,7 +9,15 @@ import { Link } from "react-router-dom";
 
 function Postcard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
-}) {
+}) 
+{
+
+    function likePost(){
+        console.log('Like post')
+    }
+    function commentOnPost() {
+      console.log("Comment on post");
+    }
   return (
     <Card fluid>
       <Card.Content>
@@ -25,12 +33,20 @@ function Postcard({
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Button as='div' labelPosition='right'>
-          <Button color='teal'>
+        <Button as='div' labelPosition='right' onClick={likePost}>
+          <Button color='teal' basic>
             <Icon name='heart' />
           </Button>
           <Label as='a' basic color='teal' pointing='left'>
             {likeCount}
+          </Label>
+        </Button>
+        <Button as='div' labelPosition='right' onClick={commentOnPost}>
+          <Button color='blue' basic>
+            <Icon name='comments' />
+          </Button>
+          <Label as='a' basic color='blue' pointing='left'>
+            {commentCount}
           </Label>
         </Button>
       </Card.Content>
