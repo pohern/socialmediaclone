5adffe67ee5fdc121e652691fc7c1d46a1c42e33
pohern <ohern_p@yahoo.com>
2021-03-07 09:23:13 -0600

    add to post details page

--- client/src/pages/PostDetails.js
+++ client/src/pages/PostDetails.js
@@ -1,8 +1,72 @@
-import React from "react";
+import React, { useContext } from "react";
 import gql from "graphql-tag";
+import { useQuery } from "@apollo/client";
+import { Button, Card, Label, Grid, Icon, Image } from "semantic-ui-react";
+import moment from "moment";
+
+import { AuthContext } from '../context/auth'
+import LikeButton from '../components/LikeButton'
 
 function SinglePost(props) {
   const postId = props.match.params.postId;
+   const { user } = useContext(AuthContext) 
+  const {
+    data: { getPost },
+  } = useQuery(FETCH_POST_QUERY, {
+    variables: {
+      postId,
+    },
+  });
+
+  let postMarkup;
+  if (!getPost) {
+    postMarkup = <p>Loading Post..</p>;
+  } else {
+    const {
+      id,
+      body,
+      createdAt,
+      username,
+      comments,
+      likes,
+      likeCount,
+      commentCount,
+    } = getPost;
+    postMarkup = (
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={2}>
+            <Image
+              floated='right'
+              size='small'
+              src='https://react.semantic-ui.com/images/avatar/large/molly.png'
+            />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Description>{body}</Card.Description>
+              </Card.Content>
+              <hr />
+              <Card.Content>
+                <LikeButton user={user} post={{ id, likeCount, likes}}/>
+                <Button as='div' labelPosition='right' onClick={() => console.log('Comment on post')}>
+                    <Button basic color='blue'>
+                        <Icon name='comments' />
+                    </Button>
+                    <Label basic color='blue' pointing='left'>
+                        {commentCount}
+                    </Label>
+                </Button>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
 }
 
 const FETCH_POST_QUERY = gql`
