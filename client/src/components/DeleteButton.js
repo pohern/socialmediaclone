23f9c2d84e2139b9d25259d10fed96cd9ce17f63
pohern <ohern_p@yahoo.com>
2021-03-08 09:17:40 -0600

    remove popp that were casuing issues with hooks

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -3,49 +3,55 @@ import gql from "graphql-tag";
 import { useMutation } from "@apollo/client";
 
 import { Button, Confirm, Icon } from "semantic-ui-react";
-import { FETCH_POSTS_QUERY } from '../util/graphql'
-function DeleteButton({ postId, callback, commentId  }) {
-    const [confirmOpen, setConfirmOpen] = useState(false)
+import { FETCH_POSTS_QUERY } from "../util/graphql";
 
-    const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION
+function DeleteButton({ postId, callback, commentId }) {
+  const [confirmOpen, setConfirmOpen] = useState(false);
 
-    const [deletePostOrMutation] = useMutation(mutation, {
-        update(proxy){
-            setConfirmOpen(false)
-            if(!commentId){
-                const data = proxy.readQuery({
-                  query: FETCH_POSTS_QUERY,
-                });
-                data.getPosts = data.getPosts.filter((p) => p.id !== postId);
-                proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
-            }
-            if(callback) callback()
-        },
-        variables: {
-            postId,
-            commentId
-        }
-    })
+  const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION;
+
+  const [deletePostOrMutation] = useMutation(mutation, {
+    update(proxy) {
+      setConfirmOpen(false);
+      if (!commentId) {
+        const data = proxy.readQuery({
+          query: FETCH_POSTS_QUERY,
+        });
+        data.getPosts = data.getPosts.filter((p) => p.id !== postId);
+        proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
+      }
+      if (callback) callback();
+    },
+    variables: {
+      postId,
+      commentId,
+    },
+  });
   return (
-      <>
-    <Button
-      as='div'
-      color='red'
-      onClick={() => setConfirmOpen(true)}
-      floated='right'
-    >
-      <Icon name='trash' style={{ margin: "0" }} />
-    </Button>
-    <Confirm open={confirmOpen} onCancel={() => setConfirmOpen(false)} onConfirm={deletePostOrMutation} />
+    <>
+      <Button
+        as='div'
+        color='red'
+        onClick={() => setConfirmOpen(true)}
+        floated='right'
+      >
+        <Icon name='trash' style={{ margin: "0" }} />
+      </Button>
+
+      <Confirm
+        open={confirmOpen}
+        onCancel={() => setConfirmOpen(false)}
+        onConfirm={deletePostOrMutation}
+      />
     </>
   );
 }
 
 const DELETE_POST_MUTATION = gql`
-    mutation deletePost($postId: ID!){
-        deletePost(postId: $postId )
-    }
-`
+  mutation deletePost($postId: ID!) {
+    deletePost(postId: $postId)
+  }
+`;
 const DELETE_COMMENT_MUTATION = gql`
   mutation deleteComment($postId: ID!, $commentId: ID!) {
     deleteComment(postId: $postId, commentId: $commentId) {
--- client/src/components/Postcard.js
+++ client/src/components/Postcard.js
@@ -3,14 +3,13 @@ import moment from "moment";
 import { Card, Icon, Label, Image, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth";
-import LikeButton from './LikeButton'
-import DeleteButton from './DeleteButton'
+import LikeButton from "./LikeButton";
+import DeleteButton from "./DeleteButton";
 
 function Postcard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) {
   const { user } = useContext(AuthContext);
-
 
   return (
     <Card fluid>
@@ -27,16 +26,18 @@ function Postcard({
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <LikeButton user={user} post={{ id, likes, likeCount }}/>
+        <LikeButton user={user} post={{ id, likes, likeCount }} />
+
         <Button labelPosition='right' as={Link} to={`/posts/${id}`}>
           <Button color='blue' basic>
             <Icon name='comments' />
           </Button>
-          <Label as='a' basic color='blue' pointing='left'>
+          <Label basic color='blue' pointing='left'>
             {commentCount}
           </Label>
         </Button>
-        {user && user.username === username && <DeleteButton postId={id}/>}
+
+        {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
   );
